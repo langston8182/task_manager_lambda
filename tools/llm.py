7d123d0e408d8964dbda8bc48c1bc63559fcf8f0
Cyril Marchive <cyril.marchive@gmail.com>
2025-04-14 12:22:51 +0200

    change llm to gpt-4o

--- tools/llm.py
+++ tools/llm.py
@@ -4,5 +4,5 @@ from langchain_openai import ChatOpenAI
 
 
 llm = ChatOpenAI(
-    temperature=0, model="gpt-4o-mini", api_key=os.environ.get("OPENAI_API_KEY")
+    temperature=0, model="gpt-4o", api_key=os.environ.get("OPENAI_API_KEY")
 )
